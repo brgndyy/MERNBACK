c0ba9cbc458a9587801fcc2417205ba6941c2c5e
brgndy <brgndy96@gmail.com>
2023-03-08 09:28:47 +0900

    sqlconnect

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
+const { sequelize } = require("./models");
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터 베이스 연결 성공.");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.use(bodyParser.json());
 
