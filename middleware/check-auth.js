58b0a468aa5dc98a72f5266ae38e3ebcc4e7d1f4
brgndy <brgndy96@gmail.com>
2023-03-21 20:56:13 +0900

    msg

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -6,12 +6,11 @@ module.exports = (req, res, next) => {
     return next();
   }
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    console.log("token : ", token);
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    console.log(token);
     if (!token) {
       throw new Error("Authentication failed!");
     }
-
     const decodedToken = jwt.verify(token, "supersecret_dont_share");
     req.userData = { userId: decodedToken.userId };
     next();
